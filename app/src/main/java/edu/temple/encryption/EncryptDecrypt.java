a02ac22bc9b8ec4344f139dbccc545c2b69b9a2c
jnickmale <tug09183@temple.edu>
2018-02-19 02:15:51 -0500

    V2

--- app/src/main/java/edu/temple/encryption/EncryptDecrypt.java
+++ app/src/main/java/edu/temple/encryption/EncryptDecrypt.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.Key;
@@ -21,6 +22,9 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.List;
 
@@ -28,11 +32,13 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 import static android.R.attr.id;
 
 public class EncryptDecrypt extends AppCompatActivity {
     private Keys keysProvider;
+    private Key initialPriv, initialPub;
     private String privKey, pubKey;
     private Key thePrivKey, thePubKey;
 
@@ -60,6 +66,8 @@ public class EncryptDecrypt extends AppCompatActivity {
                 try {
                     keyGen = KeyPairGenerator.getInstance("RSA");
                     KeyPair keys = keyGen.generateKeyPair();
+                    initialPriv = keys.getPrivate();
+                    initialPub = keys.getPublic();
                     row[0] = ((RSAPrivateKey)keys.getPrivate()).getPrivateExponent();
                     row[1] = ((RSAPublicKey)keys.getPublic()).getPublicExponent();
                 } catch (NoSuchAlgorithmException e) {
@@ -75,26 +83,49 @@ public class EncryptDecrypt extends AppCompatActivity {
                 cursor.moveToNext();
                 privKey = cursor.getString(cursor.getColumnIndex("privateKey"));
                 pubKey = cursor.getString(cursor.getColumnIndex("publicKey"));
+                cursor.close();
 
 
 
-                byte[] byteKey = Base64.decode(pubKey.getBytes(), Base64.DEFAULT);
-                X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
-                KeyFactory kf = null;
-
+                KeyFactory factory = null;
                 try {
-                    kf = KeyFactory.getInstance("RSA");
-
-                    thePubKey = kf.generatePublic(X509publicKey);
-
-                    byteKey = Base64.decode(privKey.getBytes(), Base64.DEFAULT);
-                    X509EncodedKeySpec X509privateKey = new X509EncodedKeySpec(byteKey);
-                    kf = KeyFactory.getInstance("RSA");
-
-                    thePrivKey = kf.generatePublic(X509privateKey);
-                } catch (Exception e) {
+                    factory = KeyFactory.getInstance("RSA");
+                } catch (NoSuchAlgorithmException e) {
                     e.printStackTrace();
                 }
+
+
+                RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(((RSAPublicKey)initialPub).getModulus(), new BigInteger(pubKey));
+                try {
+                    thePubKey = factory.generatePublic(rsaPublicKeySpec);
+                } catch (InvalidKeySpecException e) {
+                    e.printStackTrace();
+                }
+
+                RSAPrivateKeySpec rsaPrivateKeySpec = new RSAPrivateKeySpec(((RSAPrivateKey)initialPriv).getModulus(), new BigInteger(privKey));
+                try {
+                    thePrivKey = factory.generatePrivate(rsaPrivateKeySpec);
+                } catch (InvalidKeySpecException e) {
+                    e.printStackTrace();
+                }
+
+//                byte[] byteKey = Base64.decode(pubKey.getBytes());
+//                X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
+//                KeyFactory kf = null;
+//
+//                try {
+//                    kf = KeyFactory.getInstance("RSA");
+//
+//                    thePubKey = kf.generatePublic(X509publicKey);
+//
+//                    byteKey = Base64.decode(privKey.getBytes(), Base64.DEFAULT);
+//                    X509EncodedKeySpec X509privateKey = new X509EncodedKeySpec(byteKey);
+//                    kf = KeyFactory.getInstance("RSA");
+//
+//                    thePrivKey = kf.generatePublic(X509privateKey);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
 
             }
         });
@@ -107,8 +138,8 @@ public class EncryptDecrypt extends AppCompatActivity {
         try{
 
 
-            c = Cipher.getInstance("RSA/NONE/NoPadding");
-            c2 = Cipher.getInstance("RSA/NONE/NoPadding");
+            c = Cipher.getInstance("RSA");
+            c2 = Cipher.getInstance("RSA");
 
 
 
@@ -138,15 +169,15 @@ public class EncryptDecrypt extends AppCompatActivity {
         decryptButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                byte[] encryptedBytes;
+                byte[] decryptedBytes;
                 try {
                     c2.init(Cipher.DECRYPT_MODE, thePrivKey);
                 } catch (InvalidKeyException e) {
                     e.printStackTrace();
                 }
                 try {
-                    encryptedBytes = c2.doFinal(Base64.decode(text.getText().toString(), Base64.DEFAULT));
-                    text.setText(Base64.encodeToString(encryptedBytes, Base64.DEFAULT));
+                    decryptedBytes = c2.doFinal(Base64.decode(text.getText().toString(), Base64.DEFAULT));
+                    text.setText(Base64.encodeToString(decryptedBytes, Base64.DEFAULT));
                 } catch (IllegalBlockSizeException e) {
                     e.printStackTrace();
                 } catch (BadPaddingException e) {
